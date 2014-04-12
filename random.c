9a99f597b5ddd4d8bbc2f31dab4e860eb4d3ea65
vaclajon <jonas.vaclavek@seznam.cz>
2014-04-12 22:42:13 +0900

    sdf

--- random.c
+++ random.c
@@ -4,42 +4,57 @@
 #include <stdio.h>
 
 #define FILENAME "random.txt"
+
+void print_help(){
+    printf("some help\n");
+		exit(0);
+}
+
+
+void open_file(char *filename, FILE *file){
+      file=fopen(filename, "w");
+      if(file == NULL){
+        printf("Error opening file [%s]", filename);
+        exit(1);
+      }
+      
+}
 int main(int argc, char *argv[]){
 	srand(time(NULL));
 	int N;
 	int list[N];
 	int i;
 	FILE *output;
-	if(!strncmp(argv[1], "-h",6)){
-		printf("some help\n");
-		exit(0);
+  
+  //-h as argument
+	if(strncmp(argv[1], "-h",6) == 0){
+    print_help();
 	}
+  
+  //lenght of array
 	N = atoi(argv[1]);
-	for(i = 0;i<N;i++){
-		list[i] = rand();
-	}
-
+  
+	//checks wheather there is more than one argument
 	if(argc>2){
-		printf("%s\n", argv[2]);
-		if(!strncmp(argv[2], "-f",6)){
-			output=fopen(argv[3], "w");
+    //filename as argument		
+		if(strncmp(argv[2], "-f",6) == 0){
+			open_file(argv[3], output);
 		}
 		else{
+    
 			printf("Unsupported parameter [%s]\n",argv[2]);
 			printf("Use random -h for help\n");
 			exit(1);
 		}
 	}
-	else{
+	else{   
 		printf("Using default file name [%s]\n", FILENAME);
-		output = fopen(FILENAME, "w");
+		open_file(FILENAME, output);
 	}
-	if(output == NULL){
-		printf("Error opening file");
-		exit(1);
-	}
+  //make random and write to file	
 	for(i = 0; i < N ;i++){
-		fprintf(output,"%d ",list[i]);
+    list[i] = rand();
+		fprintf(output,"%d\n",list[i]);
 	}
 	exit(0);
 }
